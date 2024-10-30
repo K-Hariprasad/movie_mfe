c22327177c29ec185ca595ec0aec0999424ecf4c
Hariprasad <hariprasadk125@gmail.com>
2024-10-30 13:45:55 +0530

    shared modules

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,7 +1,7 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const commonConfig = require("./webpack.common");
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const devConfig = {
   mode: "development",
@@ -16,11 +16,22 @@ const devConfig = {
       template: "./public/index.html",
     }),
     new ModuleFederationPlugin({
-      name : 'container',
-      remotes : {
-        movieApp : 'movies@http://localhost:8082/remoteEntry.js'
-      }
-    })
+      name: "container",
+      remotes: {
+        movieApp: "movies@http://localhost:8082/remoteEntry.js",
+      },
+      shared: {
+        react: {
+          singleton: true,
+        },
+        "react-dom": {
+          singleton: true,
+        },
+        "react-router-dom": {
+          singleton: true,
+        },
+      },
+    }),
   ],
 };
 
--- movies/config/webpack.dev.js
+++ movies/config/webpack.dev.js
@@ -1,7 +1,7 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const commonConfig = require("./webpack.common");
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const devConfig = {
   mode: "development",
@@ -10,18 +10,32 @@ const devConfig = {
     historyApiFallback: {
       index: "/index.html",
     },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
     new ModuleFederationPlugin({
-      name : 'movies',
-      filename : 'remoteEntry.js',
-      exposes : {
-        './movieIndex': './src/App.js'
-      }
-    })
+      name: "movies",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./movieIndex": "./src/App.js",
+      },
+      shared: {
+        react: {
+          singleton: true,
+        },
+        "react-dom": {
+          singleton: true,
+        },
+        "react-router-dom": {
+          singleton: true,
+        },
+      },
+    }),
   ],
 };
 

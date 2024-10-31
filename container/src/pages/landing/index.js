98d53fa4588a535fa2b6cc77892fc31dc62c0d43
Hariprasad <hariprasadk125@gmail.com>
2024-10-31 23:06:23 +0530

    Styling

--- container/src/pages/landing/index.js
+++ container/src/pages/landing/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./styles.css";
 
 function Landing() {
   const navigate = useNavigate();
   return (
-    <div>
-      <h1>This is Landing</h1>
-      <button onClick={()=>navigate('/movies')}>Movies</button>
+    <div className="landing-page">
+      <div className="content">
+        <h1 className="app-title">Welcome to MovieApp</h1>
+        <div className="button-group">
+          <button
+            onClick={() => navigate("/movies")}
+            className="button movies-button"
+          >
+            MOVIES
+          </button>
+          <button className="button sign-in-button">SIGN IN</button>
+        </div>
+      </div>
     </div>
   );
 }
--- movies/src/pages/details/index.js
+++ movies/src/pages/details/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { movieData } from "../../data";
+import "./styles.css";
+
+const imageBasePath = "https://image.tmdb.org/t/p/w500/";
 
 function Details() {
+  const { id } = useParams();
+  const movie = movieData.results.find((i) => i.id === parseInt(id));
+
   return (
-    <div>Details</div>
-  )
+    <div className="movie-details-wrapper">
+      <img
+        src={`${imageBasePath}/${movie.poster_path}`}
+        alt={`${movie?.title} poster`}
+        className="poster"
+      />
+      <h1 className="movieDetails-title">{movie?.title}</h1>
+      <p className="overview">{movie?.overview}</p>
+    </div>
+  );
 }
 
-export default Details
+export default Details;
